6efc5b7792e8d29c26857f2a3c11e5ef550a43c2
Amber-Pittman <amber@amberpittman.com>
2020-05-01 22:17:27 -0400

    Project endpoints

--- projects/project-model.js
+++ projects/project-model.js
@@ -9,6 +9,26 @@ async function getProject() {
     return isComplete;
 }
 
+async function getProjectById(id) {
+    const project = await db("projects").where({id}).first()
+    const projectTasks = await db("task").where({project_id: id})
+
+    return {
+        ...project, 
+        completed: project.completed ? true : false,
+        tasks: projectTasks.map(task => ({
+            ...task, completed: task.completed ? true : false
+        }))
+    }
+}
+
+async function addProject(project) {
+    const [id] = await db("projects").insert(project).returning("id")
+    return await getProjectById(id)
+}
+
 module.exports = {
     getProject,
+    getProjectById,
+    addProject,
   }
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -1,15 +1,32 @@
 const express = require("express")
-const db = require("../data/config")
+const db = require("./project-model")
 
 const router = express.Router()
 
 router.get("/", async (req, res, next) => {
     try {
-        const projects = await db("projects as p")
-        
+        const projects = await db.getProjects()
         res.json(200).json(projects)
     }
     catch(err) {
+        next(err)
+    }
+})
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const project = await db.getProjectById(req.params.id)
+        return res.status(200).json(project)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post("/", (req, res, next) => {
+    try {
+        const newProject = await db.addProject(req.body)
+        return res.status(201).json(newProject)
+    } catch(err) {
         next(err)
     }
 })
